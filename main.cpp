01508615b8ca8b61be2b837d17dd1125d347f255
Md Mahbub Tanmay <129056181+MahbubTanmay@users.noreply.github.com>
2025-05-25 22:00:16 +0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -19,18 +19,25 @@ cout<<"The HexaDecimal Is: "<<hex << number <<endl;
 cout <<"To Octal Is: " <<oct<<number<<endl;
  
 
-//*********** Another Way************
-//we can directly convert string to int with different base like this
-//int_variable = stoi(string_variable , nullptr, base) ; 
+//***********************
+//here we take int number as user input
+// but what is its base? mean the number u gave what is it? 
+// 2 ways to define: 1.Through input    2.Through Program
+
+//1. Through Input
+ //User Should put 0b before number for binary, 0x for hex, 0 for octal
+
+
+//2.Using program
+ //first convert int to string using
+//string_variable = to_string(int_variable);
+ //then use below code to define number as a base
  //int_variable = stoi(string_variable , nullptr, 2) ; -- binary
  //int_variable = stoi(string_variable , nullptr, 16) ; -- Hex
 //int_variable = stoi(string_variable , nullptr, 8) ; -- oct
  //int_variable = stoi(string_variable)  ; -- Decimal
 
 
-//so convert int to string using
-//string_variable = to_string(int_variable);
-//then change base using above technique
-    
+//Check Readme.md   for more details
     return 0;
 }
